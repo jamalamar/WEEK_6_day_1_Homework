48301df11145826fdd8c05e23eb0edc94c3e7e49
Jamal Amar Bahena <jamal@Jamals-MacBook-Pro.local>
2019-01-23 15:38:07 -0600

    created array of products

---
+++ products/models.js
@@ -0,0 +1,28 @@
+
+let products = [
+
+	{	name: "ToyOne",
+		color: "Blue",
+		from: "China",
+		releaseDate: 2016
+	},
+
+	{	name: "ToyTwo",
+		color: "Green",
+		from: "China",
+		releaseDate: 2017	
+	},
+
+	{	name: "ToyThree",
+		color: "Red",
+		from: "China",
+		releaseDate: 2018
+	},
+
+	{	name: "ToyFour",
+		color: "Yellow",
+		from: "China",
+		releaseDate: 2019
+
+	}
+]
