d34559b8b35f90e7f908a1e6ba649563255122d4
Jamal Amar Bahena <jamal@Jamals-MacBook-Pro.local>
2019-01-23 15:30:56 -0600

    created array of customers on: customers models.js

---
+++ customers/models.js
@@ -0,0 +1,32 @@
+
+let customers = [
+
+	{	name: "Krista",
+		age: 20,
+		phone: 123456789,
+		city: "NY"
+	},
+
+	{	name: "Jamal",
+		age: 25,
+		phone: 354987534,
+		city: "Tokyo"	
+	},
+
+	{	name: "Ramsey",
+		age: 30,
+		phone: 987987987,
+		city: "Hong-Kong"
+	},
+
+	{	name: "Regina",
+		age: 35,
+		phone: 765765765,
+		city: "Berlin"
+
+	}
+]
+
+
+
+
